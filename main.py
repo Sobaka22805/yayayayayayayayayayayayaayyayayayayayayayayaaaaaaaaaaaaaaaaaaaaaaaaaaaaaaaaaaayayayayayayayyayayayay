2d4d719fe7a8e6333733f284bff110ae0e7877e3
student <Disl_gw81@student.itstep.org>
2024-11-23 10:10:09 +0200

    1 done logic for get object coin

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import requests
 
 print('Lesson 9: Parsing Site')
 
-responce - requests.get('https://coinmarketcap.com/')
+responce = requests.get('https://coinmarketcap.com/')
 
 if responce.status_code == 200:
 
     item_site = BeautifulSoup(responce.text, features='html.parser')
-    coin_list = item_site.findall('div', {'class', 'sc-b3fc6b7-0 dzgUIj'})
+    coins = item_site.find_all('div', {'class', 'sc-b3fc6b7-0 dzgUIj'})
 
     for coin in coins:
-        print('coin ->', coin)
+        print('coin -> ', coin)
